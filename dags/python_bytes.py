0a57c66c25b7694909c281a53921e7a56b5bd5e4
Alexander Junge <alexander.junge@gmail.com>
2021-02-05 18:27:24 +0100

    Notes

--- dags/python_bytes.py
+++ dags/python_bytes.py
@@ -1,8 +1,17 @@
 # At 17:30 daily
+# (Task 1 - fetch)
 # Fetch RSS feed
+#
+# (Task 2 - filter)
 # Look T (default: one week) back to fetch all episodes
 # Check for all episodes if already in DB based on ID
 # If not in DB, check if transcript is there
-    # If transcript there, parse everything and add to DB
+#
+# (Task 3 - store)
+# If transcript there, (parse everything and add to DB
 # Remember set: extra: { 'transcipt_added' : True }
 # Run using backfilling for past dates?
+
+# Questions
+# Xcom in taskflow -- too much data, e.g. RSS feed
+# HTTPOperator together with TaskFlow? https://airflow.apache.org/docs/apache-airflow-providers-http/stable/_api/airflow/providers/http/operators/http/index.html#airflow.providers.http.operators.http.SimpleHttpOperator
