56c35cd16d47cea558418c4a13606ae55e45e909
Alexander Junge <alexander.junge@gmail.com>
2021-02-14 17:34:44 +0100

    First implementation of task get_new_episodes

--- dags/python_bytes.py
+++ dags/python_bytes.py
@@ -1,6 +1,7 @@
 import base64
+import re
 import time
-from typing import Dict
+from typing import Dict, List
 import urllib
 
 import arrow
@@ -64,103 +65,116 @@ def fetch_transcript(episode_number, number_re, sleep_seconds=3):
     return content
 
 
-def get_recent_episodes(recent_episodes_date: DateTime) -> Dict:
+def get_recent_episodes(recent_episodes_date: DateTime) -> List[Dict]:
+    number_re = re.compile("^\d+")
+
     tree = etree.parse(
         urllib.request.urlopen("https://pythonbytes.fm/episodes/rss_full_history")
     )
     root = tree.getroot()
+    recent_episodes = []
     for item in root.iter("item"):
         tag_to_text = {}
         for child in item:
             # print("%s - %s" % (child.tag, child.text))
             tag_to_text[child.tag] = child.text
-        id_ = (
-            "PythonBytes:"
-            + tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"].strip()
-        )
-        assert len(id_) > 0
-        title = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}title"].strip()
-        assert len(title) > 0
-        author = tag_to_text[
-            "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
-        ].strip()
-        assert len(author) > 0
-        episode_number = tag_to_text[
-            "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
-        ].strip()
-        episode_number = int(episode_number)
-        d = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"].strip()
-        d = [int(x.strip()) for x in d.split(":")]
-        if len(d) == 2:
-            duration_in_seconds = d[0] * 60 + d[1]
-        elif len(d) == 3:
-            duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
-        else:
-            raise ValueError(d)
-        keywords = tag_to_text[
-            "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
-        ].strip()
-        keywords = [x.strip() for x in keywords.split(",")]
-        raw_text = tag_to_text["description"].strip()
-        assert len(raw_text) > 0
-        ## possible but not necessary here:
-        ## ensure that we separate some html elements as newlines
-        # raw_text = raw_text.replace("</div>", "\n")
-        # raw_text = raw_text.replace("</code>", "\n")
-        # raw_text = raw_text.replace("</li>", "\n")
-        # raw_text = raw_text.replace("</p>", "\n")
-        parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
-        url = tag_to_text["link"].strip()
-        publication_date = tag_to_text["pubDate"]
-        publication_date = arrow.get(publication_date, "D MMM YYYY").format(
-            "YYYY-MM-DD"
-        )
+            id_ = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
+            ].strip()
+            assert len(id_) > 0
+            id_ = "PythonBytes:" + id_
 
-        if pendulum.parse(publication_date) < recent_episodes_date:
-            # episode is too old and since episodes are sorted by publication date, we can stop here
-            break
-
-        # append transcript to both parsed and raw text
-        transcript = load_transcript(episode_number, number_re)
-        assert transcript
-        transcript = "\nEpisode transcript:\n" + transcript
-        raw_text += transcript
-        parsed_text += transcript
-
-        if submit_requests:
-            r = requests.get(
-                f"{api_url}/documents/{id_}", headers=superuser_token_headers
+            title = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}title"
+            ].strip()
+            assert len(title) > 0
+            author = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
+            ].strip()
+            assert len(author) > 0
+            episode_number = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
+            ].strip()
+            episode_number = int(episode_number)
+            d = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"
+            ].strip()
+            d = [int(x.strip()) for x in d.split(":")]
+            if len(d) == 2:
+                duration_in_seconds = d[0] * 60 + d[1]
+            elif len(d) == 3:
+                duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
+            else:
+                raise ValueError(d)
+            keywords = tag_to_text[
+                "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
+            ].strip()
+            keywords = [x.strip() for x in keywords.split(",")]
+            raw_text = tag_to_text["description"].strip()
+            assert len(raw_text) > 0
+            ## possible but not necessary here:
+            ## ensure that we separate some html elements as newlines
+            # raw_text = raw_text.replace("</div>", "\n")
+            # raw_text = raw_text.replace("</code>", "\n")
+            # raw_text = raw_text.replace("</li>", "\n")
+            # raw_text = raw_text.replace("</p>", "\n")
+            parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
+            url = tag_to_text["link"].strip()
+            publication_date = tag_to_text["pubDate"]
+            publication_date = arrow.get(publication_date, "D MMM YYYY").format(
+                "YYYY-MM-DD"
             )
-            if r.status_code == 200:
-                print(f"Exists: {id_}")
-                continue
 
-        doc_dict = {
-            "id": id_,
-            "version": "1",
-            "source": "PythonBytes",
-            "title": title,
-            "document_type": "Podcast episode",
-            "authors": [author],
-            "publication_date": publication_date,
-            "update_date": "2020-12-05",
-            "urls": [url],
-            "summary": title,
-            "raw_text": raw_text,
-            "raw_text_format": "HTML",
-            "parsed_text": parsed_text,
-            "language": "English",
-            "keywords": keywords,
-            "extra": {
-                "duration_in_seconds": duration_in_seconds,
-                "episode_number": episode_number,
-            },
-        }
-    return {}
+            if pendulum.parse(publication_date) < recent_episodes_date:
+                # episode is too old and since episodes are sorted by publication date, we can stop here
+                break
+
+            # append transcript to both parsed and raw text
+            transcript = fetch_transcript(episode_number, number_re)
+            assert transcript
+            transcript = "\nEpisode transcript:\n" + transcript
+            raw_text += transcript
+            parsed_text += transcript
+
+            doc_dict = {
+                "id": id_,
+                "version": "1",
+                "source": "PythonBytes",
+                "title": title,
+                "document_type": "Podcast episode",
+                "authors": [author],
+                "publication_date": publication_date,
+                "update_date": "2020-12-05",
+                "urls": [url],
+                "summary": title,
+                "raw_text": raw_text,
+                "raw_text_format": "HTML",
+                "parsed_text": parsed_text,
+                "language": "English",
+                "keywords": keywords,
+                "extra": {
+                    "duration_in_seconds": duration_in_seconds,
+                    "episode_number": episode_number,
+                    "has_transcript": True,
+                },
+            }
+            recent_episodes.append(doc_dict)
+    return recent_episodes
 
 
-def remove_existing_episodes(recent_episodes: Dict, token_headers: Dict) -> Dict:
-    return {}
+def remove_existing_episodes(
+    recent_episodes: List[Dict], connection: Connection, token_headers: Dict
+) -> List[Dict]:
+    new_episodes = []
+    for episode in recent_episodes:
+        id_ = episode["id"]
+        r = requests.get(f"{connection.host}/documents/{id_}", headers=token_headers)
+        if r.status_code == 200:
+            print(f"Exists: {id_}")
+            continue
+        else:
+            new_episodes.append(episode)
+    return new_episodes
 
 
 # These args will get passed on to each operator
@@ -188,7 +202,7 @@ def python_bytes_dag():
     """
 
     @task()
-    def get_new_episodes(**kwargs) -> Dict:
+    def get_new_episodes(**kwargs) -> List[Dict]:
         """
         #### Find new episodes and convert them to JSON
 
@@ -199,16 +213,18 @@ def python_bytes_dag():
         time_frame = pendulum.duration(days=int(Variable.get("podcast_recent_days")))
 
         recent_episodes_date = exec_date - time_frame
-        recent_episodes = get_recent_episodes(recent_episodes_date)
-
         api_connection_id = Variable.get("api_connection_id")
         connection = get_connection(api_connection_id)
         token_headers = get_request_headers(connection)
-        new_episodes = remove_existing_episodes(recent_episodes, token_headers)
+        recent_episodes = get_recent_episodes(recent_episodes_date)
+
+        new_episodes = remove_existing_episodes(
+            recent_episodes, connection, token_headers
+        )
         return new_episodes
 
     @task()
-    def load_episodes(new_episodes: Dict):
+    def load_episodes(new_episodes: List[Dict]):
         """
         #### Load episodes into kbase
         """
