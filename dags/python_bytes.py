d45da5d12ff217f8be254c3a3ea0ab8cb095a2cd
Alexander Junge <alexander.junge@gmail.com>
2021-02-14 16:24:21 +0100

    Towards parsing episodes

--- dags/python_bytes.py
+++ dags/python_bytes.py
@@ -1,23 +1,30 @@
 import base64
 import time
 from typing import Dict
-import json
+import urllib
 
+import arrow
 from airflow.decorators import dag, task
 from airflow.models import Connection, Variable
 from airflow.utils.dates import parse_execution_date
 from airflow.utils.session import provide_session
+from bs4 import BeautifulSoup
+from lxml import etree
 import pendulum
 from pendulum.datetime import DateTime
 import requests
 
 
 @provide_session
-def get_request_headers(connection_id: str, session=None) -> Dict:
+def get_connection(connection_id: str, session=None) -> Connection:
     connection_query = session.query(Connection).filter(
         Connection.conn_id == connection_id
     )
-    connection: Connection = connection_query.one_or_none()
+    connection = connection_query.one_or_none()
+    return connection
+
+
+def get_request_headers(connection: Connection) -> Dict:
     login_data = {
         "username": connection.login,
         "password": connection.password,
@@ -58,12 +65,101 @@ def fetch_transcript(episode_number, number_re, sleep_seconds=3):
 
 
 def get_recent_episodes(recent_episodes_date: DateTime) -> Dict:
-    token_headers = get_request_headers("dbapi")
+    tree = etree.parse(
+        urllib.request.urlopen("https://pythonbytes.fm/episodes/rss_full_history")
+    )
+    root = tree.getroot()
+    for item in root.iter("item"):
+        tag_to_text = {}
+        for child in item:
+            # print("%s - %s" % (child.tag, child.text))
+            tag_to_text[child.tag] = child.text
+        id_ = (
+            "PythonBytes:"
+            + tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"].strip()
+        )
+        assert len(id_) > 0
+        title = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}title"].strip()
+        assert len(title) > 0
+        author = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
+        ].strip()
+        assert len(author) > 0
+        episode_number = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
+        ].strip()
+        episode_number = int(episode_number)
+        d = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"].strip()
+        d = [int(x.strip()) for x in d.split(":")]
+        if len(d) == 2:
+            duration_in_seconds = d[0] * 60 + d[1]
+        elif len(d) == 3:
+            duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
+        else:
+            raise ValueError(d)
+        keywords = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
+        ].strip()
+        keywords = [x.strip() for x in keywords.split(",")]
+        raw_text = tag_to_text["description"].strip()
+        assert len(raw_text) > 0
+        ## possible but not necessary here:
+        ## ensure that we separate some html elements as newlines
+        # raw_text = raw_text.replace("</div>", "\n")
+        # raw_text = raw_text.replace("</code>", "\n")
+        # raw_text = raw_text.replace("</li>", "\n")
+        # raw_text = raw_text.replace("</p>", "\n")
+        parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
+        url = tag_to_text["link"].strip()
+        publication_date = tag_to_text["pubDate"]
+        publication_date = arrow.get(publication_date, "D MMM YYYY").format(
+            "YYYY-MM-DD"
+        )
+
+        if pendulum.parse(publication_date) < recent_episodes_date:
+            # episode is too old and since episodes are sorted by publication date, we can stop here
+            break
+
+        # append transcript to both parsed and raw text
+        transcript = load_transcript(episode_number, number_re)
+        assert transcript
+        transcript = "\nEpisode transcript:\n" + transcript
+        raw_text += transcript
+        parsed_text += transcript
+
+        if submit_requests:
+            r = requests.get(
+                f"{api_url}/documents/{id_}", headers=superuser_token_headers
+            )
+            if r.status_code == 200:
+                print(f"Exists: {id_}")
+                continue
+
+        doc_dict = {
+            "id": id_,
+            "version": "1",
+            "source": "PythonBytes",
+            "title": title,
+            "document_type": "Podcast episode",
+            "authors": [author],
+            "publication_date": publication_date,
+            "update_date": "2020-12-05",
+            "urls": [url],
+            "summary": title,
+            "raw_text": raw_text,
+            "raw_text_format": "HTML",
+            "parsed_text": parsed_text,
+            "language": "English",
+            "keywords": keywords,
+            "extra": {
+                "duration_in_seconds": duration_in_seconds,
+                "episode_number": episode_number,
+            },
+        }
     return {}
 
 
-def remove_existing_episodes(recent_episodes: Dict) -> Dict:
-    token_headers = get_request_headers("dbapi")
+def remove_existing_episodes(recent_episodes: Dict, token_headers: Dict) -> Dict:
     return {}
 
 
@@ -101,9 +197,14 @@ def python_bytes_dag():
         """
         exec_date = parse_execution_date(kwargs["execution_date"])
         time_frame = pendulum.duration(days=int(Variable.get("podcast_recent_days")))
+
         recent_episodes_date = exec_date - time_frame
         recent_episodes = get_recent_episodes(recent_episodes_date)
-        new_episodes = remove_existing_episodes(recent_episodes)
+
+        api_connection_id = Variable.get("api_connection_id")
+        connection = get_connection(api_connection_id)
+        token_headers = get_request_headers(connection)
+        new_episodes = remove_existing_episodes(recent_episodes, token_headers)
         return new_episodes
 
     @task()
@@ -111,6 +212,9 @@ def python_bytes_dag():
         """
         #### Load episodes into kbase
         """
+        api_connection_id = Variable.get("api_connection_id")
+        connection = get_connection(api_connection_id)
+        token_headers = get_request_headers(connection)
         print(new_episodes)
 
     new_episodes = get_new_episodes(execution_date="{{ execution_date }}")
