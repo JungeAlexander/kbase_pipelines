0cb9f9578d8e0b869daa4e528a001ca3edc933b5
Alexander Junge <alexander.junge@gmail.com>
2021-02-15 17:05:20 +0100

    Fix indentation error

--- dags/python_bytes.py
+++ dags/python_bytes.py
@@ -78,87 +78,81 @@ def get_recent_episodes(recent_episodes_date: DateTime) -> List[Dict]:
         for child in item:
             # print("%s - %s" % (child.tag, child.text))
             tag_to_text[child.tag] = child.text
-            id_ = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
-            ].strip()
-            assert len(id_) > 0
-            id_ = "PythonBytes:" + id_
+        id_ = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"].strip()
+        assert len(id_) > 0
+        id_ = "PythonBytes:" + id_
 
-            title = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}title"
-            ].strip()
-            assert len(title) > 0
-            author = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
-            ].strip()
-            assert len(author) > 0
-            episode_number = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
-            ].strip()
-            episode_number = int(episode_number)
-            d = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"
-            ].strip()
-            d = [int(x.strip()) for x in d.split(":")]
-            if len(d) == 2:
-                duration_in_seconds = d[0] * 60 + d[1]
-            elif len(d) == 3:
-                duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
-            else:
-                raise ValueError(d)
-            keywords = tag_to_text[
-                "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
-            ].strip()
-            keywords = [x.strip() for x in keywords.split(",")]
-            raw_text = tag_to_text["description"].strip()
-            assert len(raw_text) > 0
-            ## possible but not necessary here:
-            ## ensure that we separate some html elements as newlines
-            # raw_text = raw_text.replace("</div>", "\n")
-            # raw_text = raw_text.replace("</code>", "\n")
-            # raw_text = raw_text.replace("</li>", "\n")
-            # raw_text = raw_text.replace("</p>", "\n")
-            parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
-            url = tag_to_text["link"].strip()
-            publication_date = tag_to_text["pubDate"]
-            publication_date = arrow.get(publication_date, "D MMM YYYY").format(
-                "YYYY-MM-DD"
-            )
+        title = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}title"].strip()
+        assert len(title) > 0
+        author = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
+        ].strip()
+        assert len(author) > 0
+        episode_number = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
+        ].strip()
+        episode_number = int(episode_number)
+        d = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"].strip()
+        d = [int(x.strip()) for x in d.split(":")]
+        if len(d) == 2:
+            duration_in_seconds = d[0] * 60 + d[1]
+        elif len(d) == 3:
+            duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
+        else:
+            raise ValueError(d)
+        keywords = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
+        ].strip()
+        keywords = [x.strip() for x in keywords.split(",")]
+        raw_text = tag_to_text["description"].strip()
+        assert len(raw_text) > 0
+        ## possible but not necessary here:
+        ## ensure that we separate some html elements as newlines
+        # raw_text = raw_text.replace("</div>", "\n")
+        # raw_text = raw_text.replace("</code>", "\n")
+        # raw_text = raw_text.replace("</li>", "\n")
+        # raw_text = raw_text.replace("</p>", "\n")
+        parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
+        url = tag_to_text["link"].strip()
+        publication_date = tag_to_text["pubDate"]
+        publication_date = arrow.get(publication_date, "D MMM YYYY").format(
+            "YYYY-MM-DD"
+        )
 
-            if pendulum.parse(publication_date) < recent_episodes_date:
-                # episode is too old and since episodes are sorted by publication date, we can stop here
-                break
+        if pendulum.parse(publication_date) < recent_episodes_date:
+            # episode is too old and since episodes are sorted by publication date, we can stop here
+            break
 
-            # append transcript to both parsed and raw text
-            transcript = fetch_transcript(episode_number, number_re)
-            assert transcript
-            transcript = "\nEpisode transcript:\n" + transcript
-            raw_text += transcript
-            parsed_text += transcript
+        # append transcript to both parsed and raw text
+        transcript = fetch_transcript(episode_number, number_re)
+        assert transcript
+        transcript = "\nEpisode transcript:\n" + transcript
+        raw_text += transcript
+        parsed_text += transcript
 
-            doc_dict = {
-                "id": id_,
-                "version": "1",
-                "source": "PythonBytes",
-                "title": title,
-                "document_type": "Podcast episode",
-                "authors": [author],
-                "publication_date": publication_date,
-                "update_date": "2020-12-05",
-                "urls": [url],
-                "summary": title,
-                "raw_text": raw_text,
-                "raw_text_format": "HTML",
-                "parsed_text": parsed_text,
-                "language": "English",
-                "keywords": keywords,
-                "extra": {
-                    "duration_in_seconds": duration_in_seconds,
-                    "episode_number": episode_number,
-                    "has_transcript": True,
-                },
-            }
-            recent_episodes.append(doc_dict)
+        doc_dict = {
+            "id": id_,
+            "version": "1",
+            "source": "PythonBytes",
+            "title": title,
+            "document_type": "Podcast episode",
+            "authors": [author],
+            "publication_date": publication_date,
+            "update_date": "2020-12-05",
+            "urls": [url],
+            "summary": title,
+            "raw_text": raw_text,
+            "raw_text_format": "HTML",
+            "parsed_text": parsed_text,
+            "language": "English",
+            "keywords": keywords,
+            "extra": {
+                "duration_in_seconds": duration_in_seconds,
+                "episode_number": episode_number,
+                "has_transcript": True,
+            },
+        }
+        recent_episodes.append(doc_dict)
     return recent_episodes
 
 
