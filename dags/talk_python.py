6b6c211fa9981976bf3dbf6c9c80ab6d6d2d0beb
Alexander Junge <alexander.junge@gmail.com>
2021-03-07 09:09:55 +0100

    First draft of TalkPython DAG

--- dags/talk_python.py
+++ dags/talk_python.py
@@ -1,10 +1,15 @@
+import re
 from typing import Dict, List
+import urllib
 
+import arrow
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.utils.dates import parse_execution_date
 from bs4 import BeautifulSoup
+from lxml import etree
 import pendulum
+from pendulum.datetime import DateTime
 import requests
 
 import python_bytes
@@ -16,6 +21,105 @@ default_args = {
     "owner": "airflow",
     "retries": 0,
 }
+
+
+def get_recent_episodes(
+    recent_episodes_date: DateTime, exec_date: DateTime
+) -> List[Dict]:
+    number_re = re.compile("^\d+")
+
+    tree = etree.parse(
+        urllib.request.urlopen("https://talkpython.fm/episodes/rss_full_history")
+    )
+    root = tree.getroot()
+    recent_episodes = []
+    for item in root.iter("item"):
+        tag_to_text = {}
+        for child in item:
+            # print("%s - %s" % (child.tag, child.text))
+            tag_to_text[child.tag] = child.text
+        id_ = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"].strip()
+        assert len(id_) > 0
+        id_ = "TalkPythonToMe:" + id_
+
+        title = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}title"].strip()
+        assert len(title) > 0
+        summary = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}summary"
+        ].strip()
+        author = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}author"
+        ].strip()
+        assert len(author) > 0
+        episode_number = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode"
+        ].strip()
+        episode_number = int(episode_number)
+        d = tag_to_text["{http://www.itunes.com/dtds/podcast-1.0.dtd}duration"].strip()
+        d = [int(x.strip()) for x in d.split(":")]
+        if len(d) == 2:
+            duration_in_seconds = d[0] * 60 + d[1]
+        elif len(d) == 3:
+            duration_in_seconds = d[0] * 3600 + d[1] * 60 + d[2]
+        else:
+            raise ValueError(d)
+        keywords = tag_to_text[
+            "{http://www.itunes.com/dtds/podcast-1.0.dtd}keywords"
+        ].strip()
+        keywords = [x.strip() for x in keywords.split(",")]
+        raw_text = tag_to_text["description"].strip()
+        assert len(raw_text) > 0
+        ## possible but not necessary here:
+        ## ensure that we separate some html elements as newlines
+        # raw_text = raw_text.replace("</div>", "\n")
+        # raw_text = raw_text.replace("</code>", "\n")
+        # raw_text = raw_text.replace("</li>", "\n")
+        # raw_text = raw_text.replace("</p>", "\n")
+        parsed_text = BeautifulSoup(raw_text, "html.parser").get_text()
+        url = tag_to_text["link"].strip()
+        publication_date = tag_to_text["pubDate"]
+        publication_date = arrow.get(publication_date, "D MMM YYYY").format(
+            "YYYY-MM-DD"
+        )
+
+        if pendulum.parse(publication_date) < recent_episodes_date:
+            # episode is too old and since episodes are sorted by publication date, we can stop here
+            break
+
+        # append transcript to both parsed and raw text
+        repo_api_url = "https://api.github.com/repos/mikeckennedy/talk-python-transcripts/git/trees/master"
+        transcript = python_bytes.fetch_transcript(
+            episode_number, number_re, repo_api_url
+        )
+        assert transcript
+        transcript = "\nEpisode transcript:\n" + transcript
+        raw_text += transcript
+        parsed_text += transcript
+
+        doc_dict = {
+            "id": id_,
+            "version": "1",
+            "source": "Talk Python To Me",
+            "title": title,
+            "document_type": "Podcast episode",
+            "authors": [author],
+            "publication_date": publication_date,
+            "update_date": exec_date.to_date_string(),
+            "urls": [url],
+            "summary": summary,
+            "raw_text": raw_text,
+            "raw_text_format": "HTML",
+            "parsed_text": parsed_text,
+            "language": "English",
+            "keywords": keywords,
+            "extra": {
+                "duration_in_seconds": duration_in_seconds,
+                "episode_number": episode_number,
+                "has_transcript": True,
+            },
+        }
+        recent_episodes.append(doc_dict)
+    return recent_episodes
 
 
 @dag(
@@ -49,7 +153,7 @@ def talk_python_dag():
         api_connection_id = Variable.get("api_connection_id")
         connection = python_bytes.get_connection(api_connection_id)
         token_headers = python_bytes.get_request_headers(connection)
-        recent_episodes = get_recent_episodes(recent_episodes_date)
+        recent_episodes = get_recent_episodes(recent_episodes_date, exec_date)
 
         new_episodes = python_bytes.remove_existing_episodes(
             recent_episodes, connection, token_headers
